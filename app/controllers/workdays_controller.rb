5570dfd9564f2e8a6f15545f31f96cc8f852c7d8
Iván Wolf <ivanwofl15@gmail.com>
2021-03-04 17:59:07 -0300

    feat: add new workday

--- app/controllers/workdays_controller.rb
+++ app/controllers/workdays_controller.rb
@@ -3,6 +3,19 @@ class WorkdaysController < ApplicationController
     @workdays = Workday.all
   end
 
+  def new
+    @workday = Workday.new(work_date: Date.today)
+  end
+
+  def create
+    @product = Workday.new(product_params)
+    # if @product.save
+    #   redirect_to products_path
+    # else
+    #   render :new
+    # end
+  end
+
   private
 
   def product_params
