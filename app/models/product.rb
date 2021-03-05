56a69a9383a8782a815165ee46a74e4f95398815
Iván Wolf <ivanwofl15@gmail.com>
2021-03-04 22:13:11 -0300

    feat: delete product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
   validates :name, presence: true, allow_blank: false
   validates :price, numericality: { greater_than: 0 }
+
+  has_many :workday_products, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :products, only: %i[index new create]
+  resources :products, only: %i[index new create destroy]
   resources :workdays, only: %i[index new create show destroy]
   resources :workday_products, only: %i[new create destroy]
 end
