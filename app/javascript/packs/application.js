ff12473722d3038631ea61a242e9d3a3dc5457a2
Iván Wolf <ivanwofl15@gmail.com>
2021-03-05 13:50:06 -0300

    feat: do not include controllers

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,9 +14,9 @@ ActiveStorage.start()
 
 require("stylesheets/application.scss")
 
-import { Application } from "stimulus"
-import { definitionsFromContext } from "stimulus/webpack-helpers"
+// import { Application } from "stimulus"
+// import { definitionsFromContext } from "stimulus/webpack-helpers"
 
-const application = Application.start()
-const context = require.context("controllers", true, /\.js$/)
-application.load(definitionsFromContext(context))
+// const application = Application.start()
+// const context = require.context("controllers", true, /\.js$/)
+// application.load(definitionsFromContext(context))
