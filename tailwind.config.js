172811df23cd7ac9aaf67c96ad93c86811a1c6f6
Iván Wolf <ivanwofl15@gmail.com>
2021-03-05 13:41:21 -0300

    feat: add purge and find controllers

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
 // tailwind.config.js
 module.exports = {
-    future: {},
-    purge: [],
-    theme: {
-      extend: {},
-    },
-    variants: {},
-    plugins: [],
-  }
+  purge: [
+    "./config/initializers/simple_form.rb",
+    "./app/views/**/*.html.erb",
+    "./app/javascript/**/*.js",
+    "./app/helpers/application_helper.rb",
+  ],
+}
