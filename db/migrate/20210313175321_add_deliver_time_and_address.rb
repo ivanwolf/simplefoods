017fb3563c64a101e3831c5fcb080cd5335c3627
Iván Wolf <ivanwofl15@gmail.com>
2021-03-13 16:22:07 -0300

    feat: update migration

--- db/migrate/20210313175321_add_deliver_time_and_address.rb
+++ db/migrate/20210313175321_add_deliver_time_and_address.rb
@@ -1,7 +1,10 @@
 class AddDeliverTimeAndAddress < ActiveRecord::Migration[7.0]
   def change
     add_column :workday_products, :deliver_time, :time
-    add_column :customers, :address, :string, null: false
+    add_column :customers, :address, :string
+
+    Customer.update_all(address: "Cerro Paranal 360")
+    change_column_null :customers, :address, false
   end
 
 end
