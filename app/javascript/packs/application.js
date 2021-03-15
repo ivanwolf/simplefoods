e6909c68c64f1e4d7a0055e55390aaba8188a9f2
Iván Wolf <ivanwofl15@gmail.com>
2021-03-15 11:32:21 -0300

    feat: add copy controller

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,20 +3,20 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();
 
-require("stylesheets/application.scss")
+require("stylesheets/application.scss");
 
-// import { Application } from "stimulus"
-// import { definitionsFromContext } from "stimulus/webpack-helpers"
+import { Application } from "stimulus";
+import { definitionsFromContext } from "stimulus/webpack-helpers";
 
-// const application = Application.start()
-// const context = require.context("controllers", true, /\.js$/)
-// application.load(definitionsFromContext(context))
+const application = Application.start();
+const context = require.context("./controllers", true, /\.js$/);
+application.load(definitionsFromContext(context));
