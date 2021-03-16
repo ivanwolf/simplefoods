e502cf4976bd004af10d1b839fcefb71a5b76a0e
Iván Wolf <ivanwofl15@gmail.com>
2021-03-15 21:09:53 -0300

    feat: add title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def currency_formatter(amount)
     number_to_currency(amount, precision: 0, unit: "$", format: "%u %n")
   end
+
+  def title_with_back_button(title)
+    render partial: "components/title_with_back_button", locals: { title: title }
+  end
 end
