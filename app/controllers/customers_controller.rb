7b686bdd361313fd90fb031acd7ea0baf37ac967
Iván Wolf <ivanwofl15@gmail.com>
2021-03-22 00:49:12 -0300

    feat: use page in controller

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,7 +2,7 @@ class CustomersController < ApplicationController
   before_action :authenticate_user!
   
   def index
-    @customers = Customer.page(params[:page]).per(1)
+    @customers = Customer.page(params[:page]).per(20)
     @counter = CustomerOrderCounter.new @customers
   end
 end
