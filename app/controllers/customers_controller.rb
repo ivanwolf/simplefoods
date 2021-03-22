f9efd250ae72da43855833be1e5d43be277a592d
Iván Wolf <ivanwofl15@gmail.com>
2021-03-22 00:45:49 -0300

    feat: add kaminari to customer view

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,7 +2,7 @@ class CustomersController < ApplicationController
   before_action :authenticate_user!
   
   def index
-    @customers = Customer.all
+    @customers = Customer.page(params[:page]).per(1)
     @counter = CustomerOrderCounter.new @customers
   end
 end
