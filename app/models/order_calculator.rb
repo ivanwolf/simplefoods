25ef8d57e9bab522a276d587ce614dda495651df
Iván Wolf <ivanwofl15@gmail.com>
2021-03-22 21:10:40 -0300

    chore: add comment

--- app/models/order_calculator.rb
+++ app/models/order_calculator.rb
@@ -1,5 +1,6 @@
 # Class to compute data based on Order's data
 
+## TODO: create prducts_table model
 class OrderCalculator
   def self.total_units_taken(product_id)
     OrderProduct.joins(:workday_product).where(workday_products: {product_id: product_id}).sum(:quantity)
